cac2fc87ebca3f3b2a51cecf9d4dbc67bc2e7d36
SYED <137244394+Syed1708@users.noreply.github.com>
2025-08-24 18:38:12 +0200

    Update route.js

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { createProduct, getAllProducts } from "@/queries/product.queries"
+import { createProduct, getAllProducts } from "@/queries/product"
 
 export async function POST(req) {
   try {
